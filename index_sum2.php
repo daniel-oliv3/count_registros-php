5a616bc30fb4cc2c3f2ed1bfb8fe75c41f761d5c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-11-09 10:58:32 -0200

    Somar o valor do estoque em reais SELECT SUM(quantidade * preco_venda)

--- index_sum2.php
+++ index_sum2.php
@@ -14,7 +14,12 @@
     <?php
     
     //Somar o valor do estoque em reais
-    
+    $query_valor = "SELECT SUM(quantidade * preco_venda) AS valor_estoque FROM produtos";
+    $result_valor = $conn->prepare($query_valor);
+    $result_valor->execute();
+
+    $row_valor = $result_valor->fetch(PDO::FETCH_ASSOC);
+    echo "Valor do estoque (venda) R$: " . number_format($row_valor['valor_estoque'], 2, ",", ".") . "<br><br>";
 
     ?>
 </body>
