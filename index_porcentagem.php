a7704839c9d82e25817dd8fc00885e248abdbd43
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-11-09 12:22:18 -0200

    Aumento

--- index_porcentagem.php
+++ index_porcentagem.php
@@ -20,7 +20,14 @@
     echo "Valor do desconto R$: " . number_format($valor_desconto, 2, ",", ".") . "<br>";
 
     $prod_com_desconto = $valor_original - $valor_desconto;
-    echo "Produto com desconto R$: " . number_format($prod_com_desconto, 2, ",", ".") . "<br>";
+    echo "Produto com desconto R$: " . number_format($prod_com_desconto, 2, ",", ".") . "<br><br>";
+
+    //Acréscimo
+    $valor_acrescentado = ($valor_original * $porcentagem) / 100;
+    echo "Valor do acréscimo R$: " . number_format($valor_acrescentado, 2, ",", ".") . "<br>";
+
+    $prod_com_aumento = (($valor_original * $porcentagem) / 100) + $valor_original;
+    echo "Produto com aumento R$: " . number_format($prod_com_aumento, 2, ",", ".") . "<br>";
     ?>
 </body>
 </html>
