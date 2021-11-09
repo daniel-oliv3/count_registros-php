c42093d57d458eeab82eff013ef1cd45d5423a7f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-11-09 15:16:07 -0200

    Formulário de porcentagem

---
+++ index_form_porc.php
@@ -0,0 +1,24 @@
+<?php
+    include_once "conexao.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut ico" href="favicon.ico" type="image/x-ico">
+    <title>Sapup3 - Sum</title>
+</head>
+<body>
+    <?php
+    
+    //Formulario para calcular a porcentagem
+    $valor_orginal = 100;
+    $porcentagem = 25;
+
+    echo "Valor original do produto R$: " . number_format($valor_orginal, 2, ",", ".") . "<br><br>";
+
+    ?>
+</body>
+</html>
